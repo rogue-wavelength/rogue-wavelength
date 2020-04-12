43a3b29b06a596edb5ed3057157b6511f221460f
Jasmine Wang <jasminey.tea@gmail.com>
2020-04-12 18:09:54 -0500

    figuring out solo messages via sockets

--- client/socket.js
+++ client/socket.js
@@ -4,6 +4,14 @@ const socket = io(window.location.origin)
 
 socket.on('connect', () => {
   console.log('Connected!')
+
+  socket.on('everyone', () => {
+    console.log('everyone got this message')
+  })
+
+  socket.on('hi', (props) => {
+    console.log('omg hi', props)
+  })
 })
 
 export default socket
--- server/socket/index.js
+++ server/socket/index.js
@@ -1,9 +1,19 @@
+const store = require('../store')
+const {addPlayer} = require('../store/playerList')
+
 module.exports = (io) => {
   io.on('connection', (socket) => {
     console.log(`A socket connection to the server has been made: ${socket.id}`)
+    store.dispatch(addPlayer(socket.id))
+
+    socket.emit('everyone')
 
     socket.on('game', (props) => {
       console.log('game clicked server', props.stuff)
+      console.log(store.getState().playerList[0])
+      socket
+        .to(store.getState().playerList[0])
+        .emit('hi', 'this is the second argument')
     })
 
     socket.on('flip', (props) => {
