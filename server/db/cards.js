1e06edcc13e48731fc6113441e923ab7d59cdb08
Antanas Tricou <APTricou@gmail.com>
2020-04-12 18:25:48 -0500

    added basic cards file to export. Held on server.

---
+++ server/db/cards.js
@@ -0,0 +1,20 @@
+const cards = {
+  1: {left: 'nerd', right: 'jock'},
+  2: {left: 'young', right: 'old'},
+  3: {left: 'wet', right: 'dry'},
+  4: {left: 'little known fact', right: 'well known fact'},
+  5: {left: 'poor person', right: 'rich person'},
+  6: {left: 'cold weather', right: 'hot weather'},
+  7: {left: 'mild food', right: 'spicy food'},
+  8: {left: 'normal greeting', right: 'awkward greeting'},
+  9: {left: 'best enjoyed hot', right: 'best enjoyed cold'},
+  10: {left: 'useless', right: 'useful'},
+  11: {left: 'tastes bad', right: 'tastes good'},
+  12: {left: 'uncool', right: 'cool'},
+  13: {left: 'south', right: 'north'},
+  14: {left: 'silly', right: 'serious'},
+  15: {left: 'tired', right: 'energetic'},
+  16: {left: 'bad smell', right: 'good smell'},
+}
+
+module.exports = cards
