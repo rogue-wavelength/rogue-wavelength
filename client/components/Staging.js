c3be89bd59153d8c51db831db77b00e928217adb
Chris Pietka <chrispietka@yahoo.com>
2020-04-13 05:20:39 -0500

    added host or join options and validations

--- client/components/Staging.js
+++ client/components/Staging.js
@@ -1,39 +1,56 @@
 import React, {useState} from 'react'
 import socket from '../socket'
-import {Route, Redirect} from 'react-router-dom'
 
 const Staging = (props) => {
   const [room, setRoom] = useState('')
   const [name, setName] = useState('')
+  const [join, setJoin] = useState(true)
 
   const handleSubmit = (evt) => {
     evt.preventDefault()
-    // alert(`Entering room ${room}`)
     socket.emit('room', room)
     props.history.push(`/game/${room}?name=${name}`)
   }
 
+  const handleJoin = (evt) => {
+    evt.preventDefault()
+    setJoin(!join)
+  }
+
   return (
     <div style={{display: 'flex', justifyContent: 'center'}}>
-      <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={name}
+          onChange={(evt) => setName(evt.target.value)}
+        />
+      </label>
+      <button type="button" style={{fontSize: '2em'}}>
+        HOST
+      </button>
+      <button type="button" style={{fontSize: '2em'}} onClick={handleJoin}>
+        JOIN
+      </button>
+      <div hidden={join}>
         <label>
-          Username:
-          <input
-            type="text"
-            value={name}
-            onChange={(evt) => setName(evt.target.value)}
-          />
-        </label>
-        <label>
-          Room Code:
+          Enter Room Code:
           <input
             type="text"
             value={room}
             onChange={(evt) => setRoom(evt.target.value)}
           />
         </label>
-        <input type="submit" value="Submit" />
-      </form>
+        <button
+          type="submit"
+          style={{fontSize: '2em'}}
+          onClick={handleSubmit}
+          disabled={name.length < 1 || room.length !== 4}
+        >
+          SUBMIT
+        </button>
+      </div>
     </div>
   )
 }
