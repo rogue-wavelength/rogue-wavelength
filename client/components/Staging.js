707844deb5ab0bbf77245b79a3153927093872fb
Chris Pietka <chrispietka@yahoo.com>
2020-04-13 05:45:07 -0500

    conditional visibility

--- client/components/Staging.js
+++ client/components/Staging.js
@@ -5,6 +5,7 @@ const Staging = (props) => {
   const [room, setRoom] = useState('')
   const [name, setName] = useState('')
   const [join, setJoin] = useState(true)
+  const [host, setHost] = useState(true)
 
   const handleSubmit = (evt) => {
     evt.preventDefault()
@@ -17,23 +18,60 @@ const Staging = (props) => {
     setJoin(!join)
   }
 
+  const handleHost = (evt) => {
+    evt.preventDefault()
+    setHost(!host)
+  }
+
   return (
-    <div style={{display: 'flex', justifyContent: 'center'}}>
-      <label>
-        Username:
-        <input
-          type="text"
-          value={name}
-          onChange={(evt) => setName(evt.target.value)}
-        />
-      </label>
-      <button type="button" style={{fontSize: '2em'}}>
-        HOST
+    <div
+      style={{display: 'flex', justifyContent: 'center', flexFlow: 'column'}}
+    >
+      <button
+        hidden={!join}
+        type="button"
+        style={{fontSize: '2em'}}
+        onClick={handleHost}
+      >
+        {`${host === false ? 'MAIN MENU' : 'HOST'}`}
       </button>
-      <button type="button" style={{fontSize: '2em'}} onClick={handleJoin}>
-        JOIN
+      <div hidden={host}>
+        Host a session for your friends to join!
+        <label>
+          Username:
+          <input
+            type="text"
+            value={name}
+            onChange={(evt) => setName(evt.target.value)}
+          />
+        </label>
+        <button
+          type="submit"
+          style={{fontSize: '2em'}}
+          onClick={handleSubmit}
+          disabled={name.length < 1}
+        >
+          SUBMIT
+        </button>
+      </div>
+      <button
+        hidden={!host}
+        type="button"
+        style={{fontSize: '2em'}}
+        onClick={handleJoin}
+      >
+        {`${join === false ? 'MAIN MENU' : 'JOIN'}`}
       </button>
       <div hidden={join}>
+        Join a friend's session!
+        <label>
+          Username:
+          <input
+            type="text"
+            value={name}
+            onChange={(evt) => setName(evt.target.value)}
+          />
+        </label>
         <label>
           Enter Room Code:
           <input
