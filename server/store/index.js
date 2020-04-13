1e2b2ecffcca0eda0844df3e4df8a3742a1842f7
Jasmine Wang <jasminey.tea@gmail.com>
2020-04-12 19:58:02 -0500

    small change to redux state and import game reducer

--- server/store/index.js
+++ server/store/index.js
@@ -1,7 +1,8 @@
 const {createStore, combineReducers, applyMiddleware} = require('redux')
 const {playerList} = require('./playerList')
+const {game} = require('./game')
 
-const reducer = combineReducers({playerList})
+const reducer = combineReducers({playerList, game})
 
 const store = createStore(reducer)
 
@@ -10,11 +11,13 @@ module.exports = store
 //server side state
 const serverState = {
   playerList: [],
-  psychic: 0,
-  currentCard: ['first val', 'second val'],
-  target: 0, //0 - 100
-  clue: '',
-  teamGuess: 0, // 0 - 100
-  opponentGuess: true, // true: target < teamGuess, false: target > teamGuess
-  score: [0, 0], // teamA, teamB
+  game: {
+    psychic: 0, // index of psychic in array
+    currentCard: ['first val', 'second val'],
+    target: 0, //0 - 100
+    clue: '',
+    teamGuess: 0, // 0 - 100
+    opponentGuess: true, // true: target < teamGuess, false: target > teamGuess
+    score: [0, 0], // teamA, teamB
+  },
 }
