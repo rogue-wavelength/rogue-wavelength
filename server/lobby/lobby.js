beb8f09597707cae24352105b5ac02cfdf5ebc6b
Ian Roberts <irob466@gmail.com>
2020-04-18 08:58:06 -0500

    commented out server/lobby/ for now (unused)

--- server/lobby/lobby.js
+++ server/lobby/lobby.js
@@ -1,19 +1,19 @@
-const {makeStore} = require('../store')
+// const {makeStore} = require('../store')
 
-class Lobby {
-  constructor(code) {
-    // this makes individual stores for each room
-    this.store = makeStore()
-    this.code = code
-  }
+// class Lobby {
+//   constructor(code) {
+//     // this makes individual stores for each room
+//     this.store = makeStore()
+//     this.code = code
+//   }
 
-  /**
-   * Methods
-   */
+//   /**
+//    * Methods
+//    */
 
-  dispatch(action) {
-    return this.store.dispatch(action)
-  }
-}
+//   dispatch(action) {
+//     return this.store.dispatch(action)
+//   }
+// }
 
-export default Lobby
+// export default Lobby
